1b4e67001dce6866b01aeaab4ed34b5e2a7ab830
smitabahalkar <smitabahalkar@users.github.com>
2024-01-29 23:11:01 -0600

    react frontend code

---
+++ react-frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import UserComponent from './components/UserComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <UserComponent/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
